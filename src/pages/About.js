bb9a2cfb755b5d7010a4726b7f565cf433985587
vishalsinghhh <vishalsingh0710vs@gmail.com>
2021-08-02 14:21:17 +0530

    TheCocktailDB

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe voluptatum, cum perspiciatis laudantium enim, omnis reprehenderit consectetur inventore culpa incidunt soluta vero eveniet, consequuntur reiciendis est aut dicta dolore velit sunt! Tempora culpa alias explicabo excepturi, magnam ea vero at!</p>
+    </section>
+  )
+}
+
+export default About
